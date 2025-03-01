2fa8875246074d7272a55617cd3c4480c216abf2
Kevin8637 <kevin.lar@live.fr>
2025-03-01 12:50:38 +0100

    Add Local entity, dao and controller

---
+++ src/main/java/com/TheOffice/theOffice/entities/Local/Local.java
@@ -0,0 +1,48 @@
+package com.TheOffice.theOffice.entities.Local;
+
+import java.math.BigDecimal;
+
+public class Local {
+    private Long id;
+    private LocalLevel level;
+    private Integer size;
+    private BigDecimal rent;
+    private Integer maxEmployees;
+    private Integer maxMachines;
+    private Long id_company;
+
+    public Local() {
+    }
+
+    public Local(Long id, LocalLevel level, Integer size, BigDecimal rent, Integer maxEmployees, Integer maxMachines, Long id_company) {
+        this.id = id;
+        this.level = level;
+        this.size = size;
+        this.rent = rent;
+        this.maxEmployees = maxEmployees;
+        this.maxMachines = maxMachines;
+        this.id_company = id_company;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public LocalLevel getLevel() { return level; }
+    public void setLevel(LocalLevel level) { this.level = level; }
+
+    public Integer getSize() { return size; }
+    public void setSize(Integer size) { this.size = size; }
+
+    public BigDecimal getRent() { return rent; }
+    public void setRent(BigDecimal rent) { this.rent = rent; }
+
+    public Integer getMaxEmployees() { return maxEmployees; }
+    public void setMaxEmployees(Integer maxEmployees) { this.maxEmployees = maxEmployees; }
+
+    public Integer getMaxMachines() { return maxMachines; }
+    public void setMaxMachines(Integer maxMachines) { this.maxMachines = maxMachines; }
+
+    public Long getId_company() { return id_company; }
+    public void setId_company(Long id_company) { this.id_company = id_company; }
+}
+
