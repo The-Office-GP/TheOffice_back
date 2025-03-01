228ba6b4cc7d44e36766979eec38d4e0a6fd1d7b
Kevin8637 <kevin.lar@live.fr>
2025-03-01 12:28:46 +0100

    Initialize project

---
+++ src/test/java/com/TheOffice/theOffice/TheOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheOffice.theOffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
