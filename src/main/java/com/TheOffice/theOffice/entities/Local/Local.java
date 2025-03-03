ea0a1040332907e4c0b6af78363fc6750e79a05c
Kevin8637 <kevin.lar@live.fr>
2025-03-03 12:08:25 +0100

    Update Local init (add background_image)

--- src/main/java/com/TheOffice/theOffice/entities/Local/Local.java
+++ src/main/java/com/TheOffice/theOffice/entities/Local/Local.java
@@ -9,18 +9,20 @@ public class Local {
     private BigDecimal rent;
     private Integer maxEmployees;
     private Integer maxMachines;
+    private byte[] background_image;
     private Long id_company;
 
     public Local() {
     }
 
-    public Local(Long id, LocalLevel level, Integer size, BigDecimal rent, Integer maxEmployees, Integer maxMachines, Long id_company) {
+    public Local(Long id, LocalLevel level, Integer size, BigDecimal rent, Integer maxEmployees, Integer maxMachines, byte[] background_image, Long id_company) {
         this.id = id;
         this.level = level;
         this.size = size;
         this.rent = rent;
         this.maxEmployees = maxEmployees;
         this.maxMachines = maxMachines;
+        this.background_image = background_image;
         this.id_company = id_company;
     }
 
@@ -41,6 +43,14 @@ public class Local {
 
     public Integer getMaxMachines() { return maxMachines; }
     public void setMaxMachines(Integer maxMachines) { this.maxMachines = maxMachines; }
+
+    public byte[] getBackground_image() {
+        return background_image;
+    }
+
+    public void setBackground_image(byte[] background_image) {
+        this.background_image = background_image;
+    }
 
     public Long getId_company() { return id_company; }
     public void setId_company(Long id_company) { this.id_company = id_company; }
