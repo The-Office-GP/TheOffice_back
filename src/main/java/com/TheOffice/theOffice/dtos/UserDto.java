b34c13ec6c4fd6544870ad3a6d8f4db5b32b7677
Kevin8637 <kevin.lar@live.fr>
2025-03-11 11:19:08 +0100

    Add wallet at UserDto

--- src/main/java/com/TheOffice/theOffice/dtos/UserDto.java
+++ src/main/java/com/TheOffice/theOffice/dtos/UserDto.java
@@ -2,21 +2,25 @@ package com.TheOffice.theOffice.dtos;
 
 import com.TheOffice.theOffice.entities.User;
 
+import java.math.BigDecimal;
+
 public class UserDto {
     private long id;
     private String email;
     private String username;
     private String role;
+    private BigDecimal wallet;
 
     public UserDto() {
 
     }
 
-    public UserDto(long id, String email, String username, String role) {
+    public UserDto(long id, String email, String username, String role, BigDecimal wallet) {
         this.id = id;
         this.email = email;
         this.username = username;
         this.role = role;
+        this.wallet = wallet;
     }
 
     public long getId() {
@@ -51,7 +55,15 @@ public class UserDto {
         this.username = username;
     }
 
+    public BigDecimal getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(BigDecimal wallet) {
+        this.wallet = wallet;
+    }
+
     public static UserDto fromEntity(User user){
-        return new UserDto(user.getId(), user.getEmail(), user.getUsername(), user.getRole());
+        return new UserDto(user.getId(), user.getEmail(), user.getUsername(), user.getRole(), user.getWallet());
     }
 }
